1dbdc4200cc1a9e53394383fcd9aa01d61a2a913
Wijayaac <wijayaac25@gmail.com>
2023-11-12 09:58:06 +0800

    Update : styling for mobile screen

--- assets/product-gallery.js
+++ assets/product-gallery.js
@@ -1,18 +1,27 @@
 class ProductGallerySection {
-    constructor() {
-      const swiper = new Swiper(".gallery__container", {
-        speed: 400,
-        slidesPerView: 6,
-        spaceBetween: 10,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
+  constructor() {
+    const swiper = new Swiper(".gallery__container", {
+      speed: 400,
+      slidesPerView: 6,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 10,
         },
-      });
-    }
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+      },
+    });
   }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    new ProductGallerySection();
-  });
-  
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  new ProductGallerySection();
+});
